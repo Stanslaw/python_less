e6d6b59ce8fffdbe454a7e8854a67fdc860b813c
stanslaw <stanslaw@ya.ru>
2020-03-27 14:54:26 +0300

    Find all links

--- stepic_practical_ex/find_the_href.py
+++ stepic_practical_ex/find_the_href.py
@@ -1,15 +1,8 @@
 from urllib.request import urlopen
-import re
+from bs4 import BeautifulSoup
 
 html = urlopen('https://ru.wikipedia.org/wiki/Python').read().decode('utf-8')
-
 s = str(html)
-
-pos = s.find('<a href=')
-# print(pos)
-while pos != -1:
-    posquote = s.find('"', pos + 9)
-    href = s[pos + 9:posquote]
-    print(href)
-    pos = s.find('<a href=', pos + 1)
-    # print(pos)
+soup = BeautifulSoup(s, 'html.parser')
+for a in soup.find_all('a', href=True):
+    print(a['href'])
