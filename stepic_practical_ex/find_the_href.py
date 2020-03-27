20f78f75460b786a662d02622fa31175a04310cc
stanslaw <stanslaw@ya.ru>
2020-03-27 15:19:00 +0300

    Start Excel step

--- stepic_practical_ex/find_the_href.py
+++ stepic_practical_ex/find_the_href.py
@@ -1,8 +1,15 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('https://ru.wikipedia.org/wiki/Python').read().decode('utf-8')
+html = urlopen('https://stepik.org/media/attachments/lesson/209723/5.html').read().decode('utf-8')
 s = str(html)
 soup = BeautifulSoup(s, 'html.parser')
-for a in soup.find_all('a', href=True):
-    print(a['href'])
+print(soup.prettify())
+
+# print(list(soup.find_all("td")))
+c_sum = 0
+for i in soup.find_all("td"):
+    print(i.get_text())
+    c_sum += int(i.get_text())
+
+print(c_sum)
