48751dd5cca35da189880692119a06089efb0a07
stanslaw <stanslaw@ya.ru>
2020-03-27 15:32:45 +0300

    xlrd library

--- stepic_practical_ex/find_the_href.py
+++ stepic_practical_ex/find_the_href.py
@@ -1,15 +1,10 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+import xlrd
 
-html = urlopen('https://stepik.org/media/attachments/lesson/209723/5.html').read().decode('utf-8')
-s = str(html)
-soup = BeautifulSoup(s, 'html.parser')
-print(soup.prettify())
-
-# print(list(soup.find_all("td")))
-c_sum = 0
-for i in soup.find_all("td"):
-    print(i.get_text())
-    c_sum += int(i.get_text())
-
-print(c_sum)
+wb = xlrd.open_workbook('tab.xlsx')
+sheet_names = wb.sheet_names()
+sh = wb.sheet_by_name(sheet_names[0])
+nmin = sh.row_values(6)[2]
+for rownum in range(7, 27):
+    temp = sh.row_values(rownum)
+    nmin = min(nmin, temp[2])
+print(nmin)
