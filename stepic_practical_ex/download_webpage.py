a744ea09cb088acaae02dee6eb39c1159ee69882
stanslaw <stanslaw@ya.ru>
2020-03-27 13:19:55 +0300

    Solved ex with find max counting word

--- stepic_practical_ex/download_webpage.py
+++ stepic_practical_ex/download_webpage.py
@@ -1,7 +1,40 @@
 from urllib.request import urlopen
+import re
 
-html = urlopen('https://ru.wikipedia.org/wiki/Python').read().decode('utf-8')
+html = urlopen('https://stepik.org/media/attachments/lesson/209719/2.html').read().decode('utf-8')
 
-print(html)
+string = re.findall(r'<code>(.*?)</code>', html)
+print(string, len(string))
+print("______________")
+string2 = set(string)
 
-print(html.count("C++"))
+result = []
+max_count = 0
+for i in string2:
+    if string.count(i) > max_count:
+        result = []
+        result.append(i)
+        max_count = string.count(i)
+    elif string.count(i) == max_count:
+        result.append(i)
+
+print(result)
+
+result = sorted(result)
+
+
+print(" ".join(result))
+
+# ans = []
+# state = 0
+# for c in html:
+#     if c == '<':
+#         state = 1
+#     if c == '>':
+#         state = 0
+#     elif state == 0:
+#         ans.append(c)
+#
+# s = ''.join(ans)
+# print(s)
+# print(s.count('C++'))
