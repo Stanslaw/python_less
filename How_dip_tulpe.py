71be7edb7604dc54f23b2f9117591750565e5e59
stanslaw <stanslaw@ya.ru>
2020-03-31 14:17:48 +0300

    How dip tulpe

---
+++ How_dip_tulpe.py
@@ -0,0 +1,32 @@
+def how_deep(structure):
+    # your code here
+    text_structure = str(structure)
+    n = 0
+    max_n = 0
+    for i in text_structure:
+        if i in ['(', '[']:
+            n += 1
+        elif i in [')', ']']:
+            n -= 1
+
+        if max_n < n:
+            max_n = n
+    print(structure)
+    print(max_n)
+    return max_
+
+
+if __name__ == '__main__':
+    # print("Example:")
+    # print(how_deep((1, 2, 3)))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert how_deep((1, 2, 3)) == 1
+    assert how_deep((1, 2, (3,))) == 2
+    assert how_deep((1, 2, (3, (4,)))) == 3
+    assert how_deep(()) == 1
+    assert how_deep(((),)) == 2
+    assert how_deep((((),),)) == 3
+    assert how_deep((1, (2,), (3,))) == 2
+    assert how_deep((1, ((),), (3,))) == 3
+    print("Coding complete? Click 'Check' to earn cool rewards!")
