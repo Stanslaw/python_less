2370b6dd9f5695797786968409f19f736b04a5bf
stanslaw <stanslaw@ya.ru>
2020-04-05 17:27:29 +0300

    Point in the line 2 -  project Inside Block

--- inside_block.py
+++ inside_block.py
@@ -2,20 +2,62 @@ from random import randint
 
 def is_inside(polygon, point):
 
-    # Ключь к разгадке - количество точек пересечения луча и всех сторон фигуры
+    # Ключ к разгадке - количество точек пересечения луча и всех сторон фигуры
     # если пересечений четное количество - точка находиться вне фигуры
     # для решения надо использовать косое произведение векторов
 
-    # крайний случай, точка либо совпадает с вершиной либо лежит на каком-то отрезке - сразу True
 
+    def kosoe_proizved(a, b, offset=(0, 0)):
+        """
+        Функция реализует косое произведение векторов по координатам точек A(1,1) B(2,2) и поправочному коэффициекну
+        если обща] точка не ноль
+        """
+        a = (a[0] - offset[0], a[1] - offset[1])
+        b = (b[0] - offset[0], b[1] - offset[1])
+        kp = a[0]*b[1] - b[0]*a[1]
+        return kp
+
+    def skalarnoe_proizved(a, b, offset=(0, 0)):
+        """
+        Функция реализует скалярное произведение векторов по координатам точек A(1,1) B(2,2) и поправочному коэффициекну
+        если обща] точка не ноль
+        """
+        a = (a[0] - offset[0], a[1] - offset[1])
+        b = (b[0] - offset[0], b[1] - offset[1])
+        sp = a[0]*b[0] + a[1]*b[1]
+        return sp
+
+    def point_on_the_line(line, point):
+
+        """
+        Функция получает на вход координаты двух точек отрезка и точку проверки. Если точка лежит на отрезке
+        возвращаем True в противном случае False
+        """
+
+        # 1 если точка совпадает с крайней точкой - возвращаем True
+        if point in line:
+            # print("True")
+            return True
+
+        # 2 определяем лежит ли точка на прямой проходящей через точки line, если нет сразу возвращаем False
+        A = line[0]
+        B = line[1]
+
+        M = point
+
+        print(A, B, M)
+
+        if kosoe_proizved(B, M, A) != 0:
+            return False
+        elif skalarnoe_proizved(A, B, M) <= 0:
+            return True
+
+        return False
+
+
+    # крайний случай, точка либо совпадает с вершиной - сразу True и не проводим больше никакие рассчеты
     if point in polygon:
         return True
-
-    # точка лежит на отрезке.
-    def point_on_the_line (line, point):
-        return 0
-
-
 
 
     # Первым делом надо задать уравнения луча который проходит только через ребра фигуры
@@ -27,22 +69,43 @@ def is_inside(polygon, point):
     max_x = max(polygon, key=lambda x: x[0])
     min_y = min(polygon, key=lambda y: y[1])
     max_y = max(polygon, key=lambda y: y[1])
-    print(min_x[0], max_x[0])
-    print(min_y[1], max_y[1])
+    # print(min_x[0], max_x[0])
+    # print(min_y[1], max_y[1])
+
+    # Провекра функции
+    # print(point_on_the_line(((1, 1), (4, 1)), (2, 1)))
+
+    # print(kosoe_proizved(polygon[0], polygon[1]))
+
+    # пробегаемся по всем отрезкам по кругу и проверяем их на пересечения с точкой и с лучем от точки
+
+    new_polygon = (*polygon, polygon[0])
+    print(new_polygon)
+
+    for i in range(len(new_polygon)-1):
+        tar_a = new_polygon[i]
+        tar_b = new_polygon[i+1]
+
+        print("line -", tar_a, tar_b, "point - ", point)
+
+        # если точка лежит на одной из поверхностей - сразу принимаем решение что точка внутри фигуры
+        if point_on_the_line((tar_a, tar_b), point):
+            print("True")
+            return True
 
 
 
-
-    return True or False
+    print("False")
+    return False
 
 
 if __name__ == '__main__':
-    # assert is_inside(((1, 1), (1, 3), (3, 3), (3, 1)),
-    #                  (2, 2)) == True, "First"
+    assert is_inside(((1, 1), (1, 3), (3, 3), (3, 1)),
+                     (2, 2)) == True, "First"
     # assert is_inside(((1, 1), (1, 3), (3, 3), (3, 1)),
     #                  (4, 2)) == False, "Second"
-    assert is_inside(((1, 1), (4, 1), (2, 3)),
-                     (3, 2)) == True, "Third"
+    # assert is_inside(((1, 1), (4, 1), (2, 3)),
+    #                  (3, 2)) == True, "Third"
     # assert is_inside(((1, 1), (4, 1), (1, 3)),
     #                  (3, 3)) == False, "Fourth"
     # assert is_inside(((2, 1), (4, 1), (5, 3), (3, 4), (1, 3)),
