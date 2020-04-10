467547a71cb5b8994c35eb2a0aef5328cfc93367
stanslaw <stanslaw@ya.ru>
2020-04-10 15:27:19 +0300

    New project password 2

--- pass_with_digit.py
+++ pass_with_digit.py
@@ -1,12 +1,21 @@
 # Taken from mission Acceptable Password I
+import re
 def is_acceptable_password(password: str) -> bool:
     # your code here
+
+    # если содержит слово Password - False
+    if re.findall("password", password.lower()):
+        return False
+
+    # если только из цифр и длинна меньше 9 - False
     flag = False
     for i in password:
         if not i.isdigit():
             flag = True
-    if flag and len(password)>6 and password[-1].isdigit():
+
+    if len(password) > 9 or flag and len(password) > 6 and password[-1].isdigit():
         return True
+
     return False
 
 
@@ -16,9 +25,19 @@ if __name__ == '__main__':
 
     # These "asserts" are used for self-checking and not for an auto-testing
     assert is_acceptable_password('short') == False
-    assert is_acceptable_password('muchlonger') == False
+    assert is_acceptable_password('short54') == True
+    assert is_acceptable_password('muchlonger') == True
     assert is_acceptable_password('ashort') == False
     assert is_acceptable_password('muchlonger5') == True
     assert is_acceptable_password('sh5') == False
     assert is_acceptable_password('1234567') == False
+    assert is_acceptable_password('12345678910') == True
+    assert is_acceptable_password('password12345') == False
+    assert is_acceptable_password('PASSWORD12345') == False
+    assert is_acceptable_password('pass1234word') == True
+    assert is_acceptable_password('aaaaaa1') == False
+    assert is_acceptable_password('aaaaaabbbbb') == False
+    assert is_acceptable_password('aaaaaabb1') == True
+    assert is_acceptable_password('abc1') == False
+    assert is_acceptable_password('abbcc12') == True
     print("Coding complete? Click 'Check' to earn cool rewards!")
