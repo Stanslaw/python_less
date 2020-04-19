1432ef3b41e6d6737408da9090ee3686ac323305
stanslaw <stanslaw@ya.ru>
2020-04-19 15:26:21 +0300

    Unix_match poject. New logic with WHILE algoritm.

--- unix_math.py
+++ unix_math.py
@@ -4,37 +4,18 @@ def unix_match(filename: str, pattern: str) -> bool:
     if pattern == "*":
         return True
 
-    # Проверяем два раза. Слева направо и справа налево
-    # если оба теста показывают flag True - позвращаем True
-    flag_1 = flag_2 = False
-    for i in range(min(len(filename), len(pattern))):
-        if pattern[i] == "*":
-            print("flag_1 = True")
-            flag_1 = True
-            break
-        if filename[i] == pattern[i] or pattern[i] == "?":
-            print(filename[i], "ok")
-        else:
-            print("False")
-            return False
-    flag_1 = True
+    file_i = 0
+    pat_i = 0
+
+    while file_i < len(filename) or pat_i < len(pattern):
+        if pattern[pat_i] != "*":
+            if pattern[pat_i] == (filename[file_i] or "?"):
+                file_i += 1
+                pat_i += 1
+                continue
 
 
-    for i in range(min(len(filename), len(pattern))-1, -1, -1):
-        # print("rev = ", i)
-        if pattern[i] == "*":
-            print("flag_2 = True")
-            flag_2 = True
-            break
-        if filename[i] == pattern[i] or pattern[i] == "?":
-            print(filename[i], "ok")
-        else:
-            print("False")
-            return False
-    flag_2 = True
-
-
-    return flag_1*flag_2
+    return True
 
 
 if __name__ == '__main__':
@@ -42,10 +23,10 @@ if __name__ == '__main__':
     # print(unix_match('somefile.txt', '*'))
 
     # These "asserts" are used for self-checking and not for an auto-testing
-    assert unix_match('somefile.txt', '*') == True
-    assert unix_match('other.exe', '*') == True
+    # assert unix_match('somefile.txt', '*') == True
+    # assert unix_match('other.exe', '*') == True
     assert unix_match('my.exe', '*.txt') == False
-    assert unix_match('log1.txt', 'log?.txt') == True
-    assert unix_match('log12.txt', 'log?.txt') == False
-    assert unix_match('log12.txt', 'log??.txt') == True
+    # assert unix_match('log1.txt', 'log?.txt') == True
+    # assert unix_match('log12.txt', 'log?.txt') == False
+    # assert unix_match('log12.txt', 'log??.txt') == True
     print("Coding complete? Click 'Check' to earn cool rewards!")
