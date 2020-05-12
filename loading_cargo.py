d9491ad571d0246eeeb4285648923104386aec7d
stanslaw <stanslaw@ya.ru>
2020-05-12 17:56:44 +0300

    Random search. Try solve the cargo excercise whis random.

--- loading_cargo.py
+++ loading_cargo.py
@@ -17,18 +17,18 @@ def checkio(data):
 
         min_disbalance = tmp if min_disbalance > tmp else min_disbalance
 
-    # print(combi)
-    # print(all_sum)
-    # print(min_disbalance)
+    print(combi)
+    print(all_sum)
+    print(min_disbalance)
 
     return min_disbalance
 
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    assert checkio([10, 10]) == 0, "1st example"
-    assert checkio([10]) == 10, "2nd example"
+    # assert checkio([10, 10]) == 0, "1st example"
+    # assert checkio([10]) == 10, "2nd example"
     assert checkio([5, 8, 13, 27, 14]) == 3, "3rd example"
-    assert checkio([5, 5, 6, 5]) == 1, "4th example"
-    assert checkio([12, 30, 30, 32, 42, 49]) == 9, "5th example"
-    assert checkio([1, 1, 1, 3]) == 0, "6th example"
+    # assert checkio([5, 5, 6, 5]) == 1, "4th example"
+    # assert checkio([12, 30, 30, 32, 42, 49]) == 9, "5th example"
+    # assert checkio([1, 1, 1, 3]) == 0, "6th example"
