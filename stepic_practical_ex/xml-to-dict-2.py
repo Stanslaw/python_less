d7cabd0418559fe85e432a603fe49e7c86ee1101
stanslaw <stanslaw@ya.ru>
2020-05-21 16:35:05 +0300

    xml to dict project DONE.

--- stepic_practical_ex/xml-to-dict-2.py
+++ stepic_practical_ex/xml-to-dict-2.py
@@ -6,5 +6,13 @@ fin.close()
 
 parsedxml = xmltodict.parse(xml)
 
+all_nodes = len(parsedxml["osm"]["node"])
+nodes_with_tag = 0
 
-print(parsedxml["osm"]["node"])
+for nodes in parsedxml["osm"]["node"]:
+    if "tag" in nodes:
+        print(nodes["tag"], end="\n\n")
+        nodes_with_tag += 1
+    # print(nodes, end="\n\n")
+
+print(nodes_with_tag, all_nodes-nodes_with_tag)
