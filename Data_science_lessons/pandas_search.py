257cad8b3b5cf966446a08b97b0e00595dbe578d
stanslaw <stanslaw@ya.ru>
2020-07-08 18:10:04 +0300

    Pandas search.

--- Data_science_lessons/pandas_search.py
+++ Data_science_lessons/pandas_search.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
 ship = pd.read_csv("titanic.csv")
-print(ship)
+# print(ship)
+#
+# print(ship.shape)
+# print(ship.dtypes)
 
-print(ship.shape)
-print(ship.dtypes)
+my_data = pd.DataFrame([['A', 10], ['A', 14], ['B', 12], ['B', 23]], columns=['type', 'value'])
+
+print(my_data)
+print(my_data.type)
+
+
 
