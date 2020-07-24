b1f6331e26921bfa590612f29f6206649efbc7dc
stanslaw <stanslaw@ya.ru>
2020-07-24 19:14:39 +0300

    Reserch of IIF abonents.

--- IIF_abonents/podkl_iif.py
+++ IIF_abonents/podkl_iif.py
@@ -2,6 +2,8 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+import matplotlib.pyplot as plt
+
 data = pd.read_json('C:/Users/Stanslaw/Desktop/Jupiter/Clients_iif/result.json')
 
 data.sort_values(by='datetime', inplace=True)
@@ -22,14 +24,41 @@ data['hours'] = data['datetime'].dt.hour
 print(data)
 print(data.columns)
 
-# fig = px.scatter(data, x='datetime', y='summ')
-# fig.show()
+fig = px.scatter(data, x='datetime', y='summ')
+fig.show()
+
+# plt.style.use('ggplot')
+# data.plot(x='datetime', y='summ')
+# plt.show()
 
 
 data_2 = data.groupby('hours', as_index=False).count()
 
 print(data_2)
 
-fig = px.line(data_2, x='hours', y='datetime', title='Times of activity')
-fig.show()
 
+# fig = px.line(data_2, x='hours', y='datetime', title='Times of activity')
+# # fig.update_layout(xaxis=dict(range=[0,24]))
+# fig.update_xaxes(range=[0, 24], row=1, col=1)
+# fig.show()
+
+
+
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+
+ax.plot(data_2.datetime)
+ax.grid(True)
+
+plt.xticks(list(range(24)))
+plt.xlabel('Часы')
+plt.ylabel('Количество подключений')
+
+
+# plt.style.use('ggplot')
+# data_2.plot(x='hours', y='datetime')
+# plt.xticks(list(range(24)))
+# plt.xlabel('Часы')
+# plt.ylabel('Количество подключений')
+
+plt.show()
