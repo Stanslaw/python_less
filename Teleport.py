968facf608e6ab04b4c2b9aa57c6c95d8efd3f90
stanslaw <stanslaw@ya.ru>
2020-08-25 17:54:51 +0300

    Teleport. Start proj.

--- Teleport.py
+++ Teleport.py
@@ -1,7 +1,43 @@
 def checkio(teleports_string):
     #return any route from 1 to 1 over all points
 
-    return "123456781"
+    # Формируем из текстовой строки граф
+    graph = list(map(lambda x: (x[0], x[1]), teleports_string.split(',')))
+    graph_2 = {}
+
+    for i, j in graph:
+        if i not in graph_2:
+            graph_2[i] = set(j)
+        else:
+            graph_2[i].update(set(j))
+        if j not in graph_2:
+            graph_2[j] = set(i)
+        else:
+            graph_2[j].update(set(i))
+
+    # Находим все вершины графа
+    for i in range(len(graph)):
+        x = graph.pop(0)
+        graph.append(x[0])
+        graph.append(x[1])
+
+    print(set(graph))
+    print('\n', graph_2)
+
+
+    # Ходим по графу в поисках пути
+    def dfs(graph, start):
+        visited, stack = set(), [start]
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                stack.extend(graph[vertex] - visited)
+        return visited
+
+    print('\n', 'res -', dfs(graph_2, '1'))
+
+    return None
 
 #This part is using only for self-testing
 if __name__ == "__main__":
@@ -25,7 +61,7 @@ if __name__ == "__main__":
                 return False
         return True
 
-    assert check_solution(checkio, "12,23,34,45,56,67,78,81"), "First"
+    # assert check_solution(checkio, "12,23,34,45,56,67,78,81"), "First"
     assert check_solution(checkio, "12,28,87,71,13,14,34,35,45,46,63,65"), "Second"
-    assert check_solution(checkio, "12,15,16,23,24,28,83,85,86,87,71,74,56"), "Third"
-    assert check_solution(checkio, "13,14,23,25,34,35,47,56,58,76,68"), "Fourth"
+    # assert check_solution(checkio, "12,15,16,23,24,28,83,85,86,87,71,74,56"), "Third"
+    # assert check_solution(checkio, "13,14,23,25,34,35,47,56,58,76,68"), "Fourth"
