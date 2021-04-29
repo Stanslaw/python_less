b352c66e350bbada2ca811ff660b207f38d10d03
Stanslaw <33247204+Stanslaw@users.noreply.github.com>
2021-04-29 14:49:58 +0300

    Army battles start again

--- remove_accent.py
+++ remove_accent.py
@@ -1,20 +1,16 @@
 import unicodedata
 def checkio(in_string):
+    "remove accents"
 
     # new_str = unidecode.unidecode(in_string)
     new_str = unicodedata.normalize("NFKD", in_string)
-    only_ascii = new_str.encode('UTF-8', errors = 'ignore')
-    # only_ascii = unicodedata.normalize("NFKD", str(only_ascii)[2:-1])
-    print(in_string)
-    print(new_str)
+    only_ascii = new_str.encode('ASCII', 'ignore')
+    only_ascii = unicodedata.normalize("NFKD", str(only_ascii)[2:-1])
     print(only_ascii)
     return only_ascii
 
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-
-
+    #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    # assert checkio(u"préfèrent") == u"preferent"
-    # assert checkio(u"loài trăn lớn") == u"loai tran lon"
-    assert checkio("完好無缺") == "完好無缺"
+    assert checkio(u"préfèrent") == u"preferent"
+    assert checkio(u"loài trăn lớn") == u"loai tran lon"
     print('Done')
